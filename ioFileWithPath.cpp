ab17b76dc8580355b7218f0a720c87d71cd591a4
anytimemusic <bima220711@gmail.com>
2026-06-02 17:54:25 +0700

    kondisi jika file tdk ada

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -6,51 +6,55 @@ using namespace std;
 int main()
 {
 
-    //deklarasi variabel baris dan namafile
-    string baris, NamaFile;
+        //deklarasi variabel baris dan namafile
+        string baris, NamaFile;
 
-    //membuat input untuk menangkap nama file
-    cout << "Masukkan nama file : ";
-    cin >> NamaFile;
+        //membuat input untuk menangkap nama file
+        cout << "Masukkan nama file : ";
+        cin >> NamaFile;
 
-    //membuat file dalam mode menulis
-    ofstream outfile;
-    //menunjuk ke sebuah nama file dgn ekstensi .txt
-    outfile.open(NamaFile + ".txt", ios::out);
+        //membuat file dalam mode menulis
+        ofstream outfile;
+        //menunjuk ke sebuah nama file dgn ekstensi .txt
+        outfile.open(NamaFile + ".txt", ios::out);
 
-    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+        cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
-    //looping untuk menulis isi file
-    while (true)
-    {
+        //looping untuk menulis isi file
+        while (true)
+        {
         
-        cout << "- ";
-        //mendapatkan setiap karakter dalam satu baris
-        getline(cin, baris);
-        //kondisi jika baris hanyab berisi huruf q
-        if (baris == "q") break;
-        //menulius n memasukkan nilai dari baris ke dalam file
-        outfile << baris  << endl;
-    }
-}
-//menutup file apabila telah selesai ditulis
-outfile.close();
+            cout << "- ";
+            //mendapatkan setiap karakter dalam satu baris
+            getline(cin, baris);
+            //kondisi jika baris hanyab berisi huruf q
+            if (baris == "q") break;
+            //menulius n memasukkan nilai dari baris ke dalam file
+            outfile << baris  << endl;
+        }
 
-//membuka file dalam mode membaca
-ifstream infile;
+    //menutup file apabila telah selesai ditulis
+    outfile.close();
 
-//menunjuk ke sebuah nama file dgn ekstensi .txt
-infile.open(NamaFile + ".txt", ios::in);
+    //membuka file dalam mode membaca
+    ifstream infile;
 
-cout << endl << ">= Membaca dan membuka file" << endl;
-//kondisi jika file ada
-if (infile.is_open())
-{
-    //perulangan untuk menampilkan setiap baris
-    while (getline(infile, baris))
+    //menunjuk ke sebuah nama file dgn ekstensi .txt
+    infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">= Membaca dan membuka file" << endl;
+    //kondisi jika file ada
+    if (infile.is_open())
     {
-        cout << baris << endl;
+        //perulangan untuk menampilkan setiap baris
+        while (getline(infile, baris))
+        {
+            cout << baris << endl;
+        }
+        //menutup file jika telah selesai di loop
+        infile.close();
     }
-    //menutup file jika telah selesai di loop
-    infile.close();
+    //kondisi jika file tidak ada
+    elese cout << "unable to open file";
+    return 0;
 }
