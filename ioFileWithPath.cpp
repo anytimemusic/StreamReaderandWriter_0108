1bd9ef5b56f31daf564306ea5da9bc051df94a8d
anytimemusic <bima220711@gmail.com>
2026-06-02 17:48:17 +0700

    menutup file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -27,5 +27,13 @@ int main()
         cout << "- ";
         //mendapatkan setiap karakter dalam satu baris
         getline(cin, baris);
-        
+        //kondisi jika baris hanyab berisi huruf q
+        if (baris == "q") break;
+        //menulius n memasukkan nilai dari baris ke dalam file
+        outfile << baris  << endl;
+    }
 }
+//menutup file apabila telah selesai ditulis
+outfile.close();
+
+ifstream infile;
