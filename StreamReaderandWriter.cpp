6b9c985d72ef25ea4cca3e4fed73d60ad6d17a6f
anytimemusic <bima220711@gmail.com>
2026-06-02 18:35:00 +0700

    kondisi jika file ada adn file tidak ada

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -20,6 +20,35 @@ int main()
     while (true)
     {
         cout << "- ";
-        
+        //mendapat setiap karakter dalam satu baris
+        getline(cin, baris);
+        //kondisi false jika baris hanya berisi karakter q
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai dari variabel baris ke dalam file
+        outfile << baris << endl;
+
     }
+    //menutup file setelah penulisan selesai
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+
+    cout << endl << ">= Membaca dan membuka file" << endl;
+    //kondisi jika file ada
+    if (infile.is_open())
+    {
+        //perulangan untuk menampilkan setiap baris
+        while (getline(infile, baris))
+        {
+            cout << baris << endl;
+        }
+        //menutup file jika telah selesai
+        infile.close();
+    }
+    //kondisi jika file tidak ada
+    else cout << "unable to open file";
+    return 0;
 }
